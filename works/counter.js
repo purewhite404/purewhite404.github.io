1cf0c50658861a57f2901d7e5f18a8fb6c75c1a5
pure_white_404 <jr2hao.23soda@gmail.com>
2020-06-26 22:55:05 +0900

    タイトルも変わるようにした
    クリックするとタイトルに（2倍）と注釈が出るようになった

--- works/counter.js
+++ works/counter.js
@@ -4,4 +4,8 @@ const countUp = () => {
         count *= 2
         const countElement = document.querySelector('#count')
         countElement.innerText = '回数: ' + count
+
+        title =''
+        const initialMoveTitle = document.querySelector('#title')
+        initialMoveTitle.innerText = 'カウンターです（2倍）'
 }
